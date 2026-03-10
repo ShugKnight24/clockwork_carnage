d43f042a7326eff111f699816dded28c314ae36f
Shugmi Shumunov <SShumunov@gmail.com>
2026-03-10 05:11:36 -0400

    style: prettier formatting

--- js/main.js
+++ js/main.js
@@ -153,20 +153,19 @@ if (btnFullscreen) {
         btnFullscreen.addEventListener("click", () => {
           alert(
             "To play fullscreen on this device:\n\n" +
-              '1. Tap the Share button (↑) in Safari\n' +
+              "1. Tap the Share button (↑) in Safari\n" +
               '2. Select "Add to Home Screen"\n' +
               "3. Open Clockwork Carnage from your home screen\n\n" +
               "The game will run in fullscreen mode!",
           );
         });
       } else {
-        btnFullscreen.title =
-          "Open in Safari to add as home screen app";
+        btnFullscreen.title = "Open in Safari to add as home screen app";
         btnFullscreen.addEventListener("click", () => {
           alert(
             "To play fullscreen on this device:\n\n" +
               "1. Open this page in Safari\n" +
-              '2. Tap the Share button (↑)\n' +
+              "2. Tap the Share button (↑)\n" +
               '3. Select "Add to Home Screen"\n' +
               "4. Open Clockwork Carnage from your home screen\n\n" +
               "Note: Only Safari supports home screen apps on iOS.",
